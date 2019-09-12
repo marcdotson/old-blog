12bb4f064117e5e22e823cf0af3e071d7dbf0ebe
Marc Dotson <marc_dotson@byu.edu>
2019-09-12 14:44:15 -0600

    Code cleanup, comparison to non-centered parameterization

--- content/post/stan-hierarchical/Code/hierarchical_regression.R
+++ content/post/stan-hierarchical/Code/hierarchical_regression.R
@@ -38,23 +38,6 @@ data <- list(
   y = as.vector(sim_y)               # Vector of observations.
 )
 
-# # Check Stan code using brms.
-# get_prior(
-#   y ~ 1,
-#   data = data,
-#   family = gaussian()
-# )
-#
-# make_stancode(
-#   y ~ 1,
-#   data = data,
-#   family = gaussian(),
-#   prior = c(
-#     prior(normal(0, 5), class = Intercept),
-#     prior(cauchy(0, 2.5), class = sigma)
-#   )
-# )
-
 # Calibrate the model.
 fit <- stan(
   file = here::here("content", "post", "stan-hierarchical", "Code", "regression.stan"),
@@ -105,6 +88,111 @@ ggsave(
 
 # 01 Simple Hierarchical Regression ---------------------------------------
 # Hierarchical regression with no covariates and known variance.
+
+# Specify data and hyperparameter values.
+sim_values <- list(
+  N = 100,                            # Number of individuals.
+  K = 3,                              # Number of groups.
+  g = sample(3, 100, replace = TRUE), # Vector of group assignments.
+  mu = 5,                             # Mean of the population model.
+  tau = 1                             # Variance of the population model.
+)
+
+# Generate data.
+sim_data <- stan(
+  file = here::here("content", "post", "stan-hierarchical", "Code", "generate_data_01.stan"),
+  data = sim_values,
+  iter = 1,
+  chains = 1,
+  seed = 42,
+  algorithm = "Fixed_param"
+)
+
+# Extract simulated data and group intercepts.
+sim_y <- extract(sim_data)$y
+sim_beta <- extract(sim_data)$beta
+
+# Specify data.
+data <- list(
+  N = length(sim_y),                 # Number of individuals.
+  K = sim_values$K,                  # Number of groups.
+  y = as.vector(sim_y),              # Vector of observations.
+  g = sim_values$g                   # Vector of group assignments.
+)
+
+# Calibrate the model.
+fit <- stan(
+  file = here::here("content", "post", "stan-hierarchical", "Code", "hierarchical_regression_01.stan"),
+  data = data,
+  control = list(adapt_delta = 0.99),
+  seed = 42
+)
+
+# Diagnostics.
+source(here::here("content", "post", "stan-hierarchical", "Code", "stan_utility.R"))
+check_all_diagnostics(fit)
+
+# Check trace plots.
+fit %>%
+  mcmc_trace(
+    pars = c("mu", "tau", str_c("beta[", 1:data$K, "]")),
+    n_warmup = 500,
+    facet_args = list(nrow = 5, labeller = label_parsed)
+  )
+
+ggsave(
+  "mcmc_trace-01.png",
+  path = here::here("content", "post", "stan-hierarchical", "Figures"),
+  width = 7, height = 6, units = "in"
+)
+
+# Recover hyperparameter and parameter values.
+hyperpar_values <- tibble(
+  .variable = c("mu", "tau"),
+  values = c(sim_values$mu, sim_values$tau),
+)
+
+par_values <- tibble(
+  n = 1:data$K,
+  beta = as.vector(sim_beta)
+)
+
+fit %>%
+  gather_draws(mu, tau) %>%
+  ggplot(aes(x = .value, y = .variable)) +
+  geom_halfeyeh(.width = .95) +
+  facet_wrap(
+    ~ .variable,
+    nrow = 2,
+    scales = "free"
+  ) +
+  geom_vline(aes(xintercept = values), hyperpar_values, color = "red")
+
+ggsave(
+  "marginals-01a.png",
+  path = here::here("content", "post", "stan-hierarchical", "Figures"),
+  width = 7, height = 3, units = "in"
+)
+
+fit %>%
+  spread_draws(beta[n]) %>%
+  ggplot(aes(x = beta, y = n)) +
+  geom_halfeyeh(.width = .95) +
+  facet_wrap(
+    ~ n,
+    nrow = 3,
+    scales = "free"
+  ) +
+  geom_vline(aes(xintercept = beta), par_values, color = "red")
+
+ggsave(
+  "marginals-01b.png",
+  path = here::here("content", "post", "stan-hierarchical", "Figures"),
+  width = 7, height = 5, units = "in"
+)
+
+# 02 Hierarchical Regression ----------------------------------------------
+# Hierarchical regression with covariates and unknown variance.
 
 # Specify data and hyperparameter values.
 sim_values <- list(
@@ -234,10 +322,4 @@ ggsave(
   path = here::here("content", "post", "stan-hierarchical", "Figures"),
   width = 7, height = 5, units = "in"
 )
-
-# 02 Multiple Hierarchical Regression -------------------------------------
-# Hierarchical regression with covariates and known variance.
-
-# 03 General Hierarchical Regression --------------------------------------
-# Hierarchical regression with covariates and unknown variance.
 
